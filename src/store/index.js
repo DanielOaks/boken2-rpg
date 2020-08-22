1a3db0f627bf4ba8e3c067bae335b8605b9fe1a7
Daniel Oaks <daniel@danieloaks.net>
2020-08-22 16:38:58 +1000

    Move Store to separate folder, add some basic game data

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,38 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // hint that shows when you hover over buttons
     gameHoverHint: {
       hidden: true,
       name: '',
       description: '',
     },
+
+    // static game data
+    gameData: {
+      attributes: {
+        // for example:
+        //  str: {
+        //    fullName: 'Strength',
+        //    uiName: 'STR',
+        //  }
+      }
+    },
+
+    // transitive game state (player name, quest info, etc)
+    // this is what the save-game contains
+    gameState: {
+      player: {
+        name: '',
+        attributes: {
+          // for example:
+          //  str: {
+          //    base: 12,
+          //    mod: -3,
+          //  }
+        }
+      }
+    }
   },
   mutations: {
     showGameHoverHint (state, pl) {
