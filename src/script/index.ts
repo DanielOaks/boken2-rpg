51d67a6e2e7e6a3b46fc1e67b712ef1db9e9d9c3
Daniel Oaks <daniel@danieloaks.net>
2020-08-26 11:11:01 +1000

    we use 2 spaces in this project, thank u

--- src/script/index.ts
+++ src/script/index.ts
@@ -3,8 +3,8 @@ const A = require('arcsecond');
 
 const variableParser = A.sequenceOf([A.optionalWhitespace, A.letter, A.many(A.choice([A.letter, A.digit, A.char('.')])).map(x => (x.join(''))), A.optionalWhitespace]).map(x => (x.join('').trim()))
 .map(x => ({
-    type: 'variable',
-    value: x,
+  type: 'variable',
+  value: x,
 }));
 export const formulaParser = variableParser;
 
@@ -20,24 +20,24 @@ const plainTextMatch = A.everyCharUntil(A.choice([A.anyOfString('\\{'), A.endOfI
 // the .join('')'s are safe here because textParser will never contain any other kind of
 //  content.
 const textParser =
-A.sequenceOf([plainTextMatch,
+  A.sequenceOf([plainTextMatch,
     A.many(
-        A.choice([
-            A.sequenceOf([commentMatch, plainTextMatch]).map(x => x.join('')),
-            A.sequenceOf([escapedMatch, plainTextMatch]).map(x => x.join('')),
-        ])
+      A.choice([
+        A.sequenceOf([commentMatch, plainTextMatch]).map(x => x.join('')),
+        A.sequenceOf([escapedMatch, plainTextMatch]).map(x => x.join('')),
+      ])
     ).map(x => x.join(''))
-])
-.map(x => ({
-    type: 'plainText',
-    value: x.join(''),
+  ])
+  .map(x => ({
+  type: 'plainText',
+  value: x.join(''),
 }));
 
 
 const printParser = A.between (A.str('{{')) (A.str('}}')) (formulaParser)
 .map(x => ({
-    type: 'print',
-    value: x,
+  type: 'print',
+  value: x,
 }));
 
 // map slices off the endOfInput
